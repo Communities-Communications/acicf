d17fe732be24a2b79cf4b06e1a0fa981c1e283fa
Miguel <mikefdt@gmail.com>
2017-05-30 15:50:45 +0100

    debug

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from datetime import datetime, date
 		
 class PartnerAcicf(models.Model):
 	_inherit = 'res.partner'
@@ -14,6 +14,8 @@ class PartnerAcicf(models.Model):
 	
 	hab = fields.Char('Habilitações Literárias')
 	capital = fields.Float('% Capital Social')
+	data_nasc = fields.Date('Data de Nascimento')
+	idade = fields.Char(compute='_calculo_idade')
 	
 	formajuridica = fields.Char('Forma Jurídica da Empresa')
 	capsocial = fields.Float('Capital Social')
@@ -21,8 +23,13 @@ class PartnerAcicf(models.Model):
 
 	colab = fields.Integer('Número de Trabalhadores')	
 	tipo_empresa = fields.Char('Dimensão da Empresa :: Rever Funcionalidade', compute='_type_define')
-
-
+	
+	def _calculo_idade(self):
+		days_in_year = 365.2425
+		data = self.data_nasc.date('%Y, %m, %d')
+		age = int((date.today() - data).days / days_in_year)
+		return age
+	
 # Esta função não está a funcionar rever mais tarde	
 	@api.depends('colab')
 	def _type_define(self):
